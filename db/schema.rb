5c6164256a52442b9703aed68ffd7e034e849daa
Jason Humphries <humphriesj@gmail.com>
2014-08-01 02:19:41 -0400

    adds relationships for cars, users, comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140801055036) do
+ActiveRecord::Schema.define(version: 20140801061721) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20140801055036) do
     t.string   "model"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "comments", force: true do |t|
@@ -32,6 +33,8 @@ ActiveRecord::Schema.define(version: 20140801055036) do
     t.float    "longitude"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "car_id"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
@@ -40,6 +43,7 @@ ActiveRecord::Schema.define(version: 20140801055036) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "car_id"
   end
 
 end
