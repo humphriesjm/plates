bc4ce11e662446b0a11ede76e90460b857a1dceb
Jason Humphries <humphriesj@gmail.com>
2014-08-12 01:46:46 -0400

    fixes error

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,6 +2,6 @@ class Car < ActiveRecord::Base
   belongs_to :user
   has_many :comments
   
-  validates :car, presence: true, uniqueness: {case_sensitive: false}
+  validates :plate, presence: true, uniqueness: {case_sensitive: false}
   
 end
